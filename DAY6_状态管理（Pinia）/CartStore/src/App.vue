3d14f681d0df01e30beebcbae7bb2531395e71a4
jiaaaa26 <2974679029@qq.com>
2025-05-11 19:59:12 +0800

    second save

--- DAY6_状态管理（Pinia）/CartStore/src/App.vue
+++ DAY6_状态管理（Pinia）/CartStore/src/App.vue
@@ -10,6 +10,7 @@
 import UserLogin from "./components/UserLogin.vue";
 import ProductList from "./components/ProductList.vue";
 import Cart from "./components/Cart.vue";
+const first = 1;
 </script>
 
 <style scoped></style>
